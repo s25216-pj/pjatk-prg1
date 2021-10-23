d0bcec19e6fb16841d64aa58f8cb116da88c0d38
s25216 <s25216@pjwstk.edu.pl>
2021-10-23 16:41:16 +0200

    added sub, add, div, mul files

---
+++ src/s01-add.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std :: cout , std :: cerr , and std :: cin
+#include <string>
+
+auto main ( int argc , char * argv []) -> int
+{
+    if (argc == 0) {
+        return 1;
+    }
+    auto const a = std::stoi (argv [1]);
+    auto const b = std::stoi (argv [2]);
+    std::cout << (a + b ) << "\n ";
+    return 0;
+}
