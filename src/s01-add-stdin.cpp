147bbb2937d13dfbdcb65657f4176dca9dd31a52
s25216 <s25216@pjwstk.edu.pl>
2021-10-23 16:52:29 +0200

    added sub, add, div, mul with stdin

---
+++ src/s01-add-stdin.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // for std :: cout , std :: cerr , and std :: cin
+#include <string>
+
+auto ask_user_for_integer ( std :: string prompt ) -> int
+{
+    std::cout << prompt << " int :";
+    auto n = std::string {};
+    std::getline ( std::cin , n );
+    return std::stoi (n );
+}
+
+auto main () -> int
+{
+    auto const a = ask_user_for_integer (" a = ");
+    auto const b = ask_user_for_integer (" b = ");
+    std::cout << (a + b ) << "\n ";
+    return 0;
+}
